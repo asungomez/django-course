be1ce1ce6b1e67128b951a5482dc05c68dc15d14
Asun Gómez <asun.gomez@wazuh.com>
2025-07-09 12:03:41 +0200

    Refresh token (#8)
    * retrieve and store the refresh token
    
    * Use refresh token to retrieve new access token
    
    * Fix typing issues
    
    * Fix comments

--- src/core/auth.py
+++ src/core/auth.py
@@ -1,12 +1,12 @@
 import json
-from typing import Optional
+from typing import Optional, Tuple
 
 import requests
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.middleware import AuthenticationMiddleware
 from django.contrib.auth.models import AnonymousUser
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 from core.crypto import Crypto
 from user.serializers import UserSerializer
@@ -19,45 +19,56 @@ class TokenManager:
     Class to manage Okta authentication and token handling
     """
 
-    def get_email_from_token(self, token: str) -> str:
+    def authenticate(
+        self,
+        access_token: str,
+        refresh_token: str
+    ) -> Tuple[str, str, str]:
         """
-        Get the email from the token. It decodes the token and retrieves
-        the email from the claims.
+        Get the email from the token. If the access token is invalid or
+        expired, it will use the refresh token to get a new access token.
 
         :param token: The JWT token received from Okta
 
-        :return: The email address from the token
+        :return: The email address from the token, the access token, and the
+        refresh token
         """
         # In testing environments we don't use real Okta, so the token is
         # just a JSON string containing the claims
         if settings.MOCK_AUTH:
             # Decode the token as a JSON string
-            decoded_token = json.loads(token)
+            decoded_token = json.loads(access_token)
             # Get the email from the decoded token
             mock_email: str = decoded_token.get('sub')
-            return mock_email
+            return mock_email, access_token, refresh_token
 
         url = f"{settings.OKTA['DOMAIN']}/userinfo"
         headers = {
             "Accept": "application/json",
-            "Authorization": f"Bearer {token}"
+            "Authorization": f"Bearer {access_token}"
         }
         response = requests.get(url, headers=headers)
+        if response.status_code == 401:
+            access_token = self.get_access_token_from_refresh_token(
+                refresh_token
+                )
+            headers["Authorization"] = f"Bearer {access_token}"
+            response = requests.get(url, headers=headers)
         response.raise_for_status()
         userinfo = response.json()
         email: str = userinfo.get("email")
         if not email:
             raise ValueError("Email not found in the token")
-        return email
+        return email, access_token, refresh_token
 
-    def get_access_token(self, code: str) -> str:
+    def get_tokens_from_provider(self, code: str) -> Tuple[str, str]:
         """
         Make a request to Okta to retrieve the access token based on the
         code
 
         :param code: The authorization code received from Okta
 
-        :return: The access token
+        :return: The access token and refresh token
         """
         url = f"{settings.OKTA['DOMAIN']}/oauth/token"
         headers = {
@@ -73,17 +84,20 @@ class TokenManager:
         }
         response = requests.post(url, headers=headers, data=payload)
         response.raise_for_status()
-        access_token: str = response.json()["access_token"]
-        return access_token
+        access_token: str = response.json().get("access_token")
+        refresh_token: str = response.json().get("refresh_token")
+        return access_token, refresh_token
 
-    def get_token_from_request(self, request: HttpRequest) -> Optional[str]:
+    def get_tokens_from_request(self, request: HttpRequest) -> Tuple[
+        Optional[str], Optional[str]
+    ]:
         """
-        Get the token from the request. It checks both the cookies and the
+        Get the tokens from the request. It checks both the cookies and the
         Authorization header.
 
         :param request: The request object
 
-        :return: The token if found, otherwise None
+        :return: A tuple containing the access token and refresh token, or None
         """
         encrypted_credentials = request.COOKIES.get(
             settings.AUTH_COOKIE_CONFIG["NAME"]
@@ -93,13 +107,70 @@ class TokenManager:
             if auth_header:
                 header_parts = auth_header.split(" ")
                 if len(header_parts) >= 2 and header_parts[0] == "Bearer":
-                    token = " ".join(header_parts[1:])
+                    access_token = " ".join(header_parts[1:])
+                    refresh_token = 'placeholder_refresh_token'
         else:
             crypto = Crypto()
             credentials_json = crypto.decrypt(encrypted_credentials)
             credentials = json.loads(credentials_json)
-            token = credentials.get("access_token")
-        return token
+            access_token = credentials.get("access_token")
+            refresh_token = credentials.get("refresh_token")
+        return access_token, refresh_token
+
+    def get_access_token_from_refresh_token(
+        self, refresh_token: str
+    ) -> str:
+        """
+        Get a new access token using the refresh token
+        :param refresh_token: The refresh token to use for getting a new
+        access token
+        :return: The new access token
+        """
+        url = f"{settings.OKTA['DOMAIN']}/oauth/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+        }
+        payload = {
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "client_id": settings.OKTA["CLIENT_ID"],
+            "client_secret": settings.OKTA["CLIENT_SECRET"]
+        }
+        response = requests.post(url, headers=headers, data=payload)
+        response.raise_for_status()
+        access_token: Optional[str] = response.json().get("access_token")
+        if not access_token:
+            raise ValueError("Access token not found in the response")
+        return access_token
+
+    def set_credentials_as_cookie(self, response: HttpResponse,
+                                  access_token: str,
+                                  refresh_token: str) -> None:
+        """
+        Set the access and refresh tokens as a cookie in the response
+        :param response: The response object
+        :param access_token: The access token to set in the cookie
+        :param refresh_token: The refresh token to set in the cookie
+        """
+        crypto = Crypto()
+        credentials_map = {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+        }
+        credentials = json.dumps(credentials_map)
+        encrypted_credentials = crypto.encrypt(credentials)
+
+        response.set_cookie(
+            key=settings.AUTH_COOKIE_CONFIG["NAME"],
+            value=encrypted_credentials,
+            domain=settings.AUTH_COOKIE_CONFIG["DOMAIN"],
+            path=settings.AUTH_COOKIE_CONFIG["PATH"],
+            expires=settings.AUTH_COOKIE_CONFIG["LIFETIME"],
+            secure=settings.AUTH_COOKIE_CONFIG["SECURE"],
+            httponly=settings.AUTH_COOKIE_CONFIG["HTTP_ONLY"],
+            samesite=settings.AUTH_COOKIE_CONFIG["SAMESITE"],
+        )
 
 
 class CustomAuthMiddleware(AuthenticationMiddleware):
@@ -109,6 +180,8 @@ class CustomAuthMiddleware(AuthenticationMiddleware):
 
     verifier = TokenManager()
     serializer = UserSerializer()
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
 
     def process_request(self, request: HttpRequest) -> None:
         """
@@ -117,13 +190,32 @@ class CustomAuthMiddleware(AuthenticationMiddleware):
         :param request: The request object
         """
         try:
-            token = self.verifier.get_token_from_request(request)
-            if token is None:
+            at, rt = self.verifier.get_tokens_from_request(request)
+            if at is None or rt is None:
                 request.user = AnonymousUser()
                 return
-            email = self.verifier.get_email_from_token(token)
+            self.access_token = at
+            self.refresh_token = rt
+            email, at, rt = self.verifier.authenticate(at, rt)
             user = self.serializer.find_by_email(email)
             request.user = user
         except Exception as e:
             print(f"Authentication failed: {str(e)}")
             request.user = AnonymousUser()
+
+    def process_response(
+        self,
+        _: HttpRequest,
+        response: HttpResponse
+    ) -> HttpResponse:
+        """
+        Process the response to set the access and refresh tokens as cookies.
+        :param _: The request object (not used)
+        :param response: The response object
+        :return: The response object with the cookies set
+        """
+        if self.access_token and self.refresh_token:
+            self.verifier.set_credentials_as_cookie(
+                response, self.access_token, self.refresh_token
+            )
+        return response
--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest, HttpResponse, JsonResponse
@@ -7,7 +5,6 @@ from django.shortcuts import redirect
 from django.views import View
 
 from core.auth import TokenManager
-from core.crypto import Crypto
 
 from .serializers import UserSerializer
 
@@ -24,30 +21,15 @@ class LoginView(View):
             return redirect(f"{settings.FRONT_END_URL}/error")
         try:
             token_manager = TokenManager()
-            access_token = token_manager.get_access_token(code)
-            email = token_manager.get_email_from_token(access_token)
+            at, rt = token_manager.get_tokens_from_provider(code)
+            email, at, rt = token_manager.authenticate(at, rt)
             try:
                 self.serializer.find_by_email(email)
             except User.DoesNotExist:
                 self.serializer.create({"email": email})
 
-            crypto = Crypto()
-            credentials_map = {
-                "access_token": access_token,
-            }
-            credentials = json.dumps(credentials_map)
-            encrypted_credentials = crypto.encrypt(credentials)
             response = redirect(f"{settings.FRONT_END_URL}/profiles")
-            response.set_cookie(
-                key=settings.AUTH_COOKIE_CONFIG["NAME"],
-                value=encrypted_credentials,
-                domain=settings.AUTH_COOKIE_CONFIG["DOMAIN"],
-                path=settings.AUTH_COOKIE_CONFIG["PATH"],
-                expires=settings.AUTH_COOKIE_CONFIG["LIFETIME"],
-                secure=settings.AUTH_COOKIE_CONFIG["SECURE"],
-                httponly=settings.AUTH_COOKIE_CONFIG["HTTP_ONLY"],
-                samesite=settings.AUTH_COOKIE_CONFIG["SAMESITE"],
-            )
+            token_manager.set_credentials_as_cookie(response, at, rt)
             return response
         except Exception as e:
             print(e)
